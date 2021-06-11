295402695963251332dbcfe8f3db54ab889f3f71
Jugal-Ahuja <ahujajugal92@gmail.com>
2021-06-11 19:04:30 +0530

    Lint Issue Fix

--- mtn-loginpage-app/src/app/app.component.ts
+++ mtn-loginpage-app/src/app/app.component.ts
@@ -5,10 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'mtn-loginpage-app';
 
-  ngOnInit() {
-  
-  }
 }
